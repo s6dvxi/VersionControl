43439463c463288e4d385dd20390982877fe5fb8
s6dvxi <gergely.pocsai2@stud.uni-corvinus.hu>
2022-10-07 12:37:35 +0200

    load function

--- ExcelGenerator/ExcelGenerator/Form1.cs
+++ ExcelGenerator/ExcelGenerator/Form1.cs
@@ -12,9 +12,18 @@ namespace ExcelGenerator
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> flats;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            flats = context.Flat.ToList();
         }
     }
 }
