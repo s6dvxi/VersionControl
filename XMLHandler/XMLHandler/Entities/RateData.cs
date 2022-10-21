c8481e020c1d9417267fe0089318a266fa444205
s6dvxi <gergely.pocsai2@stud.uni-corvinus.hu>
2022-10-21 12:19:50 +0200

    creating currency list

---
+++ XMLHandler/XMLHandler/Entities/RateData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLHandler
+{
+    class RateData
+    {
+        DateTime Date { get; set; }
+        string Currency { get; set; }
+        decimal Value { get; set; }
+    }
+}
--- XMLHandler/XMLHandler/Form1.cs
+++ XMLHandler/XMLHandler/Form1.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using XMLHandler.MNBServiceReference;
 
 namespace XMLHandler
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = Rates;
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
