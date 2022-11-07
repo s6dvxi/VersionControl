ffe195ce651566d7a90e925a9398f07f43ae43b7
s6dvxi <gergely.pocsai2@stud.uni-corvinus.hu>
2022-11-07 21:50:47 +0100

    initializing present clas

---
+++ Gyak8_Patterns/Gyak8_Patterns/Present.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyak8_Patterns
+{
+    class Present : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
