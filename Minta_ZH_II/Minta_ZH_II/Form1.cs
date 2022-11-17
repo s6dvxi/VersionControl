2a4b53a1d75cc3c5e4213126b7cfee5f514d7a67
s6dvxi <gergely.pocsai2@stud.uni-corvinus.hu>
2022-11-17 19:57:49 +0100

    xml loading

--- Minta_ZH_II/Minta_ZH_II/Form1.cs
+++ Minta_ZH_II/Minta_ZH_II/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Minta_ZH_II
 {
@@ -16,6 +18,50 @@ namespace Minta_ZH_II
         public Form1()
         {
             InitializeComponent();
+            GetProducts();
+        }
+
+        private string GetXML(string filename)
+        {
+            using(var sr = new StreamReader(filename, Encoding.Default))
+            {
+                var xml = sr.ReadToEnd();
+                return xml;
+            }
+        }
+
+        private void GetProducts()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(GetXML("Menu.xml"));
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var name = element.SelectSingleNode("name").InnerText;
+                var calories = element.SelectSingleNode("calories").InnerText;
+                var description = element.SelectSingleNode("description").InnerText;
+                var type = element.SelectSingleNode("type").InnerText;
+
+                if (type == "food")
+                {
+                    var p = new Food()
+                    {
+                        Title = name,
+                        Calories = int.Parse(calories),
+                        Description = description
+                    };
+                    _products.Add(p);
+                }
+                else
+                {
+                    var p = new Drink
+                    {
+                        Title = name,
+                        Calories = int.Parse(calories)
+                    };
+                    _products.Add(p);
+                }
+            }
         }
     }
 }
