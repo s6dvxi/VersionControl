23ca31e2cde33bf506b5bf2108a284cd1fb304aa
s6dvxi <gergely.pocsai2@stud.uni-corvinus.hu>
2022-11-25 13:11:25 +0100

    class finished

--- UnitTestExample/UnitTestExample/Controllers/AccountController.cs
+++ UnitTestExample/UnitTestExample/Controllers/AccountController.cs
@@ -27,7 +27,7 @@ namespace UnitTestExample.Controllers
                     "A megadott e-mail cím nem megfelelő!");
             if(!ValidateEmail(email))
                 throw new ValidationException(
-                    "A megadottt jelszó nem megfelelő!\n" +
+                    "A megadott jelszó nem megfelelő!\n" +
                     "A jelszó legalább 8 karakter hosszú kell legyen, csak az angol ABC betűiből és számokból állhat, és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot.");
 
             var account = new Account()
@@ -50,7 +50,9 @@ namespace UnitTestExample.Controllers
 
         public bool ValidatePassword(string password)
         {
-            return true;
+            return Regex.IsMatch(
+                password,
+                @"[A-z0-9]{}");
         }
     }
 }
